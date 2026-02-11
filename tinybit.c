ce9553db55cd67368956bedd70013b7b16ac4fa1
MeesCode <meesgamez@gmail.com>
2026-02-11 22:30:13 +0100

    lua used pre allocated memory

--- tinybit.c
+++ tinybit.c
@@ -29,7 +29,81 @@ static pngle_t *pngle;
 static int (*gamecount_func)();
 static void (*gameload_func)(int index);
 
-int ud;
+// Static memory pool for Lua allocator
+#define LUA_HEAP_SIZE (1024 * 50)
+static uint8_t lua_heap[LUA_HEAP_SIZE];
+
+typedef struct BlockHeader {
+    size_t size;  // usable size (excluding header)
+    bool free;
+} BlockHeader;
+
+#define BLOCK_HDR_SIZE (sizeof(BlockHeader))
+#define MIN_BLOCK_SIZE 8
+
+static bool lua_heap_initialized = false;
+static size_t lua_heap_used = 0;
+
+static void lua_heap_init(void) {
+    BlockHeader *first = (BlockHeader *)lua_heap;
+    first->size = LUA_HEAP_SIZE - BLOCK_HDR_SIZE;
+    first->free = true;
+    lua_heap_initialized = true;
+    lua_heap_used = 0;
+}
+
+static void *pool_alloc(size_t size) {
+    // align to pointer size
+    size = (size + sizeof(void*) - 1) & ~(sizeof(void*) - 1);
+
+    uint8_t *pos = lua_heap;
+    while (pos < lua_heap + LUA_HEAP_SIZE) {
+        BlockHeader *hdr = (BlockHeader *)pos;
+        if (hdr->free && hdr->size >= size) {
+            // split if remaining space is large enough
+            if (hdr->size >= size + BLOCK_HDR_SIZE + MIN_BLOCK_SIZE) {
+                BlockHeader *next = (BlockHeader *)(pos + BLOCK_HDR_SIZE + size);
+                next->size = hdr->size - size - BLOCK_HDR_SIZE;
+                next->free = true;
+                hdr->size = size;
+            }
+            hdr->free = false;
+            lua_heap_used += hdr->size;
+            return pos + BLOCK_HDR_SIZE;
+        }
+        pos += BLOCK_HDR_SIZE + hdr->size;
+    }
+    return NULL; // out of memory
+}
+
+static void pool_free(void *ptr) {
+    if (!ptr) return;
+
+    BlockHeader *hdr = (BlockHeader *)((uint8_t *)ptr - BLOCK_HDR_SIZE);
+    hdr->free = true;
+    lua_heap_used -= hdr->size;
+
+    // coalesce with next block
+    uint8_t *next_pos = (uint8_t *)ptr + hdr->size;
+    if (next_pos < lua_heap + LUA_HEAP_SIZE) {
+        BlockHeader *next = (BlockHeader *)next_pos;
+        if (next->free) {
+            hdr->size += BLOCK_HDR_SIZE + next->size;
+        }
+    }
+
+    // coalesce with previous block (scan from start)
+    uint8_t *pos = lua_heap;
+    while (pos < (uint8_t *)hdr) {
+        BlockHeader *prev = (BlockHeader *)pos;
+        uint8_t *prev_end = pos + BLOCK_HDR_SIZE + prev->size;
+        if (prev_end == (uint8_t *)hdr && prev->free) {
+            prev->size += BLOCK_HDR_SIZE + hdr->size;
+            break;
+        }
+        pos = prev_end;
+    }
+}
 
 static void (*frame_func)();
 static void (*input_func)();
@@ -145,36 +219,29 @@ int lua_gameload(lua_State* L) {
     return 0;
 }
 
-void *l_alloc_restricted (void *ud, void *ptr, size_t osize, size_t nsize){
-    const int MAX_SIZE = 1024*50; /* set limit here */
-    int *used = (int *)ud;
+void *l_alloc_pool(void *ud, void *ptr, size_t osize, size_t nsize) {
+    (void)ud;
 
-    if(ptr == NULL) {
-        /*
-        * <http://www.lua.org/manual/5.2/manual.html#lua_Alloc>:
-        * When ptr is NULL, osize encodes the kind of object that Lua is
-        * allocating.
-        *
-        * Since we don’t care about that, just mark it as 0.
-        */
-        osize = 0;
+    if (!lua_heap_initialized) {
+        lua_heap_init();
     }
 
-    if (nsize == 0)
-    {
-        free(ptr);
-        *used -= osize; /* substract old size from used memory */
+    if (nsize == 0) {
+        pool_free(ptr);
         return NULL;
     }
-    else
-    {
-        if (*used + (nsize - osize) > MAX_SIZE) /* too much memory in use */
-        return NULL;
-        ptr = realloc(ptr, nsize);
-        if (ptr) /* reallocation successful? */
-        *used += (nsize - osize);
-        return ptr;
+
+    if (ptr == NULL) {
+        return pool_alloc(nsize);
     }
+
+    // realloc: allocate new block, copy, free old
+    void *new_ptr = pool_alloc(nsize);
+    if (new_ptr) {
+        memcpy(new_ptr, ptr, osize < nsize ? osize : nsize);
+        pool_free(ptr);
+    }
+    return new_ptr;
 }
 
 // Initialize the TinyBit system with memory, input state, and audio buffer pointers
@@ -195,7 +262,7 @@ void tinybit_init(struct TinyBitMemory* memory, bool* button_state_ptr) {
     tb_audio_init();
     
     // set up lua VM
-    L = lua_newstate(l_alloc_restricted, &ud);
+    L = lua_newstate(l_alloc_pool, NULL);
     lua_setup(L);
 
     // add special functions to lua for reading game files
@@ -306,7 +373,7 @@ void tinybit_loop() {
     display_time = get_ticks_ms_func() - start_time;
     start_time += display_time;
 
-    printf("used memory: %d bytes\n", ud);
+    printf("used memory: %d bytes\n", lua_heap_used);
 
     // printf("[TinyBit] Frame time: %d ms (render: %d ms, display: %d ms, audio: %d ms)\n", get_ticks_ms_func() - frame_time, render_time, display_time, audio_time);
 }
